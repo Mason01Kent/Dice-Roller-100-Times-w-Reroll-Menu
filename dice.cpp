5e96dc63e4975a1f68123afee0fb6e30bbc4a78d
Mason01Kent <109551910+Mason01Kent@users.noreply.github.com>
2023-11-14 21:18:06 -0500

    Cleaned up project, added a menu for rereolling the dice and exitng

--- dice.cpp
+++ dice.cpp
@@ -24,8 +24,34 @@ int main() {
         counts[rollResult - 1]++; // Increment the count for the rolled number
     }
 
-    for (int i = 0; i < 6; ++i) {
-        std::cout << "The number " << (i + 1) << " was rolled " << counts[i] << " times" << std::endl;
+    for (int i = 0; i < 6; ++i) {//prints out the number of times each number was rolled
+        cout << "\t" << (i + 1) << ")" << " was rolled " << counts[i] << " times" << std::endl;
+    }
+    cout << endl << "Thank you for using the Dice Roller!" << endl;
+    cout << " ~ Mason Kent ~" << endl;
+}
+
+int main() {
+
+    cout << "Project 6" << endl << "Welcome to the Dice Roller!" << endl << endl << "Press 1 to roll the dice! or 2 to quit" << endl;
+
+    int choice;
+    //input validation
+    while (!(cin >> choice) || choice < 1 || choice > 2) {
+        cout << "Invalid input. Please enter a number between 1 and 5: ";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+
+    //switch to determine input and what inputs to ask and outputs to provide
+    switch (choice) {
+    case 1:
+        CallDice();
+        break;
+
+    case 2:
+        break;
+    }
     }
 
     return 0;
